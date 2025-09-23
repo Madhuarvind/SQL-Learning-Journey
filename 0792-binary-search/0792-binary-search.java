f3b18614d1f907e40ebd72cfbe70c2861630910e
Madhuaravind P <166808473+Madhuarvind@users.noreply.github.com>
2025-09-23 16:14:17 +0530

    Time: 0 ms (100%), Space: 45.7 MB (61.65%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int idx = Arrays.binarySearch(nums, target);
+        return idx >= 0 ? idx : -1;
+    }
+}
